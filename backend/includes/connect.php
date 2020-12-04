234bca99213a63532f9311c5086f88b8730f5bac
beltonmany <44243447+beltonmany@users.noreply.github.com>
2020-12-04 12:19:55 -0500

    Update connect.php

--- backend/includes/connect.php
+++ backend/includes/connect.php
@@ -1,5 +1,5 @@
 <?php
-$tsaConn = new mysqli("localhost", "aeneasb_boop", "sungisveryawesome1", "aeneasb_TSA");
+$tsaConn = new mysqli("localhost", /*user*/, /*pass*/, /*database*/);
 if(mysqli_connect_errno())
     echo "Connection failed: ".mysqli_connect_error();
 
